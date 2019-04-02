1e2cadd419ce80dcb9a95f0425417b89e097efcb
Jacqi Entzminger <jackie.entz@gmail.com>
2019-04-02 05:43:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
-    "<h1>Buster's Best Bowling Alley</h1>"
-  end 
-    
     get '/' do
       erb :index
     end
+ 
+  get '/' do
+    "<h1>Buster's Best Bowling Alley</h1>"
+  end
+ 
 end
